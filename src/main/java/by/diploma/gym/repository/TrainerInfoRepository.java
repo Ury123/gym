069b8618073f470dacb797b2fdc151f67af2ac1d
Yury Bykouski <100156710+Ury123@users.noreply.github.com>
2025-04-13 01:59:46 +0300

    Delete src/main/java/by/diploma/gym/repository/TrainerInfoRepository.java
    delete

--- src/main/java/by/diploma/gym/repository/TrainerInfoRepository.java
+++
@@ -1,9 +0,0 @@
-package by.diploma.gym.repository;
-
-import by.diploma.gym.model.TrainerInfo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface TrainerInfoRepository extends JpaRepository<TrainerInfo, UUID> {
-}
