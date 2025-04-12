872847e4ce07f2987eac932953bca42c44d1b348
Yury Bykouski <100156710+Ury123@users.noreply.github.com>
2025-04-13 01:59:15 +0300

    Delete src/main/java/by/diploma/gym/repository/SubscriptionRepository.java
    delete

--- src/main/java/by/diploma/gym/repository/SubscriptionRepository.java
+++
@@ -1,9 +0,0 @@
-package by.diploma.gym.repository;
-
-import by.diploma.gym.model.Subscription;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface SubscriptionRepository extends JpaRepository<Subscription, UUID> {
-}
